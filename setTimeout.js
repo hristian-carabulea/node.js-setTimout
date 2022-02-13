cebdda9595231564bcf5913134c50b16ed7018d7
Hristian Carabulea <hristian.carabulea@outlook.com>
2022-02-13 17:25:29 +0100

    first commmit

---
+++ setTimeout.js
@@ -0,0 +1,8 @@
+setTimeout(function() { console.log("One second is gone.")
+}, 1000);
+
+setTimeout(() => { console.log("Two seconds are gone.")
+}, 2000);
+
+setTimeout(() => { console.log("Three seconds are gone.")
+}, 3000);
